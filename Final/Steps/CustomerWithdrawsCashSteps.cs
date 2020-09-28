e0f4bcb72f02a56f3b1e0122519d41a03b6407de
Darek <darek.kacban@gmail.com>
2020-09-28 23:55:39 +0200

    Add solution with 3 projects and readme file

---
+++ Final/Steps/CustomerWithdrawsCashSteps.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Bank;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using ATM = Bank.ATM;
+
+namespace BDDExampleProject.Features
+{
+    [Binding]
+    public class CustomerWithdrawsCashSteps
+    {
+        public ATM Atm { get; set; } = new ATM();
+
+        [Given(@"the account is in credit")]
+        public void GivenTheAccountIsInCredit()
+        {
+            Atm.Balance = 5000;
+        }
+        
+        [Given(@"the card is valid")]
+        public void GivenTheCardIsValid()
+        {
+
+            Atm.Card = new Card();
+            Atm.Card.IsValid = true;
+            Atm.IsCardInserted = true;
+        }
+        
+        [Given(@"the dispenser contains cash")]
+        public void GivenTheDispenserContainsCash()
+        {
+            Atm.DispenserContainsCash = true;
+        }
+        
+        [Given(@"the account is overdrawn")]
+        public void GivenTheAccountIsOverdrawn()
+        {
+            Atm.Balance = 0;
+        }
+        
+        [When(@"the customer requests cash")]
+        public void WhenTheCustomerRequestsCash()
+        {
+            Atm.RequestCash(1000);
+        }
+        
+        [Then(@"ensure the account is debited")]
+        public void ThenEnsureTheAccountIsDebited()
+        {
+            Assert.IsTrue(Atm.Balance > 0);
+        }
+        
+        [Then(@"ensure cash is dispensed")]
+        public void ThenEnsureCashIsDispensed()
+        {
+            Assert.IsTrue(Atm.IsCashDispended);
+        }
+        
+        [Then(@"ensure the card is returned")]
+        public void ThenEnsureTheCardIsReturned()
+        {
+            Assert.IsFalse(Atm.IsCardInserted);
+        }
+        
+        [Then(@"ensure a rejection message is displayed")]
+        public void ThenEnsureARejectionMessageIsDisplayed()
+        {
+            Assert.IsTrue(Atm.IsRejectionMessageDisplayed);
+        }
+        
+        [Then(@"ensure cash is not dispensed")]
+        public void ThenEnsureCashIsNotDispensed()
+        {
+            Assert.IsFalse(Atm.IsCashDispended);
+        }
+    }
+}
